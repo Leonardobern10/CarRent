a6a3d24c8e16a387081d856d0643f6bf0527a725
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-05-13 21:09:07 -0300

    feat: criação de componente do tipo select

---
+++ src/components/Select.tsx
@@ -0,0 +1,37 @@
+import type { ReactElement } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectLabel,
+} from "../components/ui/select";
+import { Button } from "./ui/button";
+
+export default function SelectComponent(props: {
+  placeholder: string;
+  selectLabel: string;
+  value: Array<string>;
+}): ReactElement {
+  return (
+    <Select>
+      <SelectTrigger className="hover:text-black text-lg w-fit justify-start text-left bg-white-background p-6 h-12 font-montserrat">
+        <Button variant={"ghost"}>
+          <SelectValue className="text-black" placeholder={props.placeholder} />
+        </Button>
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>{props.selectLabel}</SelectLabel>
+          {props.value.map((el, index) => (
+            <SelectItem key={index} value={el}>
+              {el}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
