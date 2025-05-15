3b48a218543d5a0b80e617acc1455a8ef25d0e94
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-05-15 10:53:04 -0300

    fix: corrigindo mensagem exibida

--- src/data/dataAboutUs.ts
+++ src/data/dataAboutUs.ts
@@ -1,5 +1,5 @@
 import type { DATA_ABOUTUS } from "@/model/DataAboutUsType";
 
 export const dataAbout: DATA_ABOUTUS = {
-  TEXT: "We are a specialized team committed to providing reliable car rental services. One of the advantages of renting a car from us is offering competitive and transparent prices. By providing services such as comprehensive insurance....",
+  TEXT: "We are a specialized team committed to providing reliable car rental services. One of the advantages of renting a car from us is offering competitive and transparent prices. By providing services such as comprehensive insurance...",
 };
