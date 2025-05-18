6a17d341b34a5cfe52403d2ec804c12bbd593afc
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-05-17 22:29:39 -0300

    fix: padronizando a reutilização para tailwindcss

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,17 +7,20 @@ export default function Button(props: {
   fontSize?: string;
   backgroundColor?: string;
   color?: string;
+  weight?: string;
 }): ReactElement {
   return (
     <button
       style={{
-        width: `${props.width}`,
-        height: `${props.height}`,
-        fontSize: `${props.fontSize}`,
         backgroundColor: `${props.backgroundColor}`,
-        color: `${props.color}`,
       }}
-      className="bg-orange-primary rounded-sm p-2 text-sm font-poppins font-bold text-black-medium hover:cursor-pointer hover:bg-black-medium hover:text-bege transition-colors"
+      className={`bg-orange-primary rounded-sm p-2 text-${
+        props.fontSize ?? "sm"
+      } font-${props.weight} text-${
+        props.color
+      } font-poppins font-bold text-black-medium w-${props.width} h-${
+        props.height
+      } hover:cursor-pointer hover:bg-black-medium hover:text-bege transition-colors`}
     >
       {props.buttonName}
     </button>
