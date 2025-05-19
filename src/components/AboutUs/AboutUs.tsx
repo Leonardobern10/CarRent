8de666b5e57f2c606621156cd731ca6406fb8644
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-05-19 19:40:57 -0300

    fix: removendo imagem car_orange

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -6,11 +6,11 @@ import TitleSection from "../TitleSection";
 
 export default function AboutUs(): ReactElement {
   return (
-    <section className="flex flex-col justify-around md:justify-center h-full w-full max-md:bg-black-medium row-span-9 md:row-span-5 col-span-7 md:col-start-2 md:col-end-7">
+    <section className="flex flex-col justify-around md:justify-center h-full w-full max-md:bg-black-medium row-span-7 md:row-span-5 col-span-7 md:col-start-2 md:col-end-7">
       <div className="flex flex-col items-center justify-center md:gap-y-10">
         <TitleSection white={true} text="About Us" />
         <div className="w-full flex flex-col-reverse md:flex-row items-center justify-center gap-x-16 gap-y-10 mt-8">
-          <div className="h-full w-fit">
+          <div className="h-full w-fit max-md:hidden">
             <img
               src="src/assets/images/car_orange.png"
               alt=""
