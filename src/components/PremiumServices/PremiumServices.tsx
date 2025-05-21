9427525c6cf05a465cf0b5db2e0b8005186caf03
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-05-21 12:09:30 -0300

    fix: corringindo tamanho do texto

--- src/components/PremiumServices/PremiumServices.tsx
+++ src/components/PremiumServices/PremiumServices.tsx
@@ -10,7 +10,7 @@ export default function PremiumServices(): ReactElement {
     <section className="font-poppins col-span-7 row-span-6 md:row-span-5 flex flex-col justify-evenly pt-10 md:pt-5 items-center md:gap-y-5 md:bg-linear-to-b md:to-70% md:to-black-white md:from-70% md:from-black-medium max-md:bg-black-medium">
       <TitleSection white={true} text="Serviços premmium" />
       <div className="w-4/6 h-5/6 flex flex-col justify-between md:justify-evenly items-center gap-y-10">
-        <p className="md:w-3/5 text-white-background text-sm/8 md:text-lg/8 text-center font-extralight">
+        <p className="md:w-3/4 text-white-background text-sm/8 md:text-lg/8 text-center font-extralight">
           A Carent é uma empresa de aluguel de carros respeitável que oferece
           uma ampla variedade de serviços úteis para todos os gostos.
         </p>
@@ -28,7 +28,7 @@ export default function PremiumServices(): ReactElement {
           <Service
             icon={MdOutlineGpsFixed}
             service="GPS nos Carros"
-            description="Todos os carros estão equipados com sistema de navegação GPS."
+            description="Carros estão equipados com sistema de navegação GPS."
           />
         </div>
       </div>
