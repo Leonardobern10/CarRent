571624accc8df15bb764c266698bf4289f5cde56
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-05-21 12:03:30 -0300

    fix: atribuindo layout diretamente ao objeto

--- src/components/Presentation/Main.tsx
+++ src/components/Presentation/Main.tsx
@@ -32,7 +32,7 @@ export default function Main(props: { data: DATA_PRESENTATION }): ReactElement {
       className="flex items-center h-[70%] main-background text-white-dark font-poppins mt-7 "
     >
       <div className="flex flex-row justify-evenly items-center w-[47%] z-1 h-[80%]">
-        <div className="w-[10%] h-[70%] flex flex-col items-center gap-y-5">
+        <div className="w-[10%] h-[90%] flex flex-col items-center gap-y-5">
           <VerticalLine />
           <ContainerIconsNetwork icons={icons} />
           <VerticalLine />
