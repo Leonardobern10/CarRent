25b8caa5342f15456f057e2516fdac3fec3efa63
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-05-23 17:03:17 -0300

    config: resolvendo bug do tipo IntersectionObserver do Framer Motion

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,15 @@
 import "@testing-library/jest-dom";
+
+class IntersectionObserverMock {
+  constructor(callback: any, options?: any) {}
+
+  observe = () => {};
+  unobserve = () => {};
+  disconnect = () => {};
+}
+
+Object.defineProperty(window, "IntersectionObserver", {
+  writable: true,
+  configurable: true,
+  value: IntersectionObserverMock,
+});
