194a87f81f813fa4c2c29e113816d0179fba5a2f
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-05-24 10:49:39 -0300

    feat: inserindo rotas para navegação

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import "./assets/styles/main.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <StrictMode>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </StrictMode>
+  </BrowserRouter>
 );
