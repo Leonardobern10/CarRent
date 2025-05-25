874f3aca78c86cb1c9ce176067c5f357137846e9
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-05-24 22:23:57 -0300

    feat: permitindo voltar à Home pela Logo

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,11 +1,12 @@
 import type { ReactElement } from "react";
+import { Link } from "react-router";
 
 export default function Logo(props: { left?: boolean }): ReactElement {
   return (
     <div
       className={`logo ${props.left ? "text-left" : "text-center"} w-fit h-fit`}
     >
-      CARENT
+      <Link to="/">CARENT</Link>
     </div>
   );
 }
