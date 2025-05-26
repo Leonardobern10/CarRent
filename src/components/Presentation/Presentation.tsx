2fa62b8d9ab695ee2c530f501e36b8a33229b493
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-05-26 20:28:00 -0300

    modified:   src/components/Presentation/Presentation.tsx

--- src/components/Presentation/Presentation.tsx
+++ src/components/Presentation/Presentation.tsx
@@ -11,7 +11,7 @@ export default function Presentation(props: {
   const screenWidth: number = useWindowWidth();
 
   return (
-    <div className="flex flex-col h-screen bg-blend-color-dodge bg-cover bg-center col-span-7 row-span-3 md:row-span-3 lg:row-span-5 bg-black-medium lg:bg-gradient-to-l lg:from-60% lg:from-white lg:to-60% lg:to-black lg:mt-15">
+    <div className="flex flex-col bg-blend-color-dodge bg-cover bg-center col-span-7 row-span-3 md:row-span-3 lg:row-span-5 bg-black-medium lg:bg-gradient-to-l lg:from-60% lg:from-white lg:to-60% lg:to-black lg:mt-15">
       {screenWidth >= 1024 ? (
         <Main data={props.data} path="/carros" />
       ) : (
