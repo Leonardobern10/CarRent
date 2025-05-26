59b3186fa630f8da198d1bebc21a1dfe15897c73
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-05-25 22:13:11 -0300

    fix: corrigindo animação com hover

--- src/components/App/Header/ItemHeader.tsx
+++ src/components/App/Header/ItemHeader.tsx
@@ -9,7 +9,7 @@ export default function ItemHeader(props: {
   const width = useWindowWidth();
   const Icon = props.icon;
   return (
-    <li className="max-lg:flex max-lg:flex-row max-lg:space-evenly max-lg:justify-center max-lg:w-fit max-lg:gap-x-2 list-none px-2 tracking-tight text-lg font-normal hover:cursor-pointer lg:hover:shadow-2xs lg:hover:shadow-bege lg:active:text-bege">
+    <li className="max-lg:flex max-lg:flex-row max-lg:space-evenly max-lg:justify-center max-lg:w-fit max-lg:gap-x-2 list-none px-2 tracking-tight text-lg font-normal hover:cursor-pointer lg:hover:shadow-2xs lg:hover:text-bege lg:active:text-bege">
       {width < 1024 && (
         <span aria-label="Página inicial">
           <Icon className="h-full w-full" />
