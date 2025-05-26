de4340413da3bdabaa08a79f5c1e37f8b1fdbd90
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-05-26 18:10:09 -0300

    fix: padronizando imagens

--- src/data/dataCarAd.ts
+++ src/data/dataCarAd.ts
@@ -2,8 +2,6 @@ import type { CarInfo } from "@/model/types/CarInfoType";
 import { BsCarFront, BsFuelPump } from "react-icons/bs";
 import { VscPersonAdd } from "react-icons/vsc";
 import car1 from "../assets/images/jeep_1.png";
-import car2 from "../assets/images/lamborguini.png";
-import car3 from "../assets/images/mini_cooper.png";
 import car4 from "../assets/images/nivus_1.png";
 import car5 from "../assets/images/sentra_1.png";
 
@@ -18,28 +16,6 @@ export const allDataCars: Array<CarInfo> = [
     ],
     image: car1,
     brand: "JEEP",
-  },
-  {
-    name: "Ferrari Enzo",
-    dailyRate: 340,
-    labels: [
-      { icon: VscPersonAdd, content: "2" },
-      { icon: BsFuelPump, content: "Elétrico" },
-      { icon: BsCarFront, content: "Sedan" },
-    ],
-    image: car2,
-    brand: "FERRARI",
-  },
-  {
-    name: "Fiat Copé",
-    dailyRate: 167,
-    labels: [
-      { icon: VscPersonAdd, content: "4" },
-      { icon: BsFuelPump, content: "Gasolina" },
-      { icon: BsCarFront, content: "Coupé" },
-    ],
-    image: car3,
-    brand: "FIAT",
   },
   {
     name: "Fiat Nivus 1.0",
